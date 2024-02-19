d80c1e442f02761b7846a65ecb8c34485e0fc006
Jorge <jorgeharanda97@yahoo.com>
2024-02-18 16:17:53 -0800

    feat: initial footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full text-center text-text_color mt-auto h-12'>
-        This is the footer!
+    <div className='w-full text-center text-text_color mt-auto h-16 py-6'>
+        iamjorge © 2024
     </div>
   )
 }
